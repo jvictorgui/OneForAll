ff4c9fb65d0e8a8bb2c231e7a1af6645ac17f7f6
joao <druvao136@gmail.com>
2023-07-31 19:40:50 -0300

    req 3

---
+++ challenges/desafio3.sql
@@ -0,0 +1,14 @@
+SELECT
+  ud.user_name AS pessoa_usuaria,
+  COUNT(sp.song_id) AS musicas_ouvidas,
+  ROUND(SUM(s.song_duration) / 60, 2) AS total_minutos
+FROM
+  user_data ud
+LEFT JOIN
+  songs_playback sp ON ud.user_id = sp.user_id
+LEFT JOIN
+  songs s ON sp.song_id = s.song_id
+GROUP BY
+  ud.user_name
+ORDER BY
+  ud.user_name;
